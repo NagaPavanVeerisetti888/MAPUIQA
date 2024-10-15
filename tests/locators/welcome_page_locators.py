12a344039839387c9e9ff361a9337290e743941d
Naga Pavan Veerisetti <63100374+NagaPavanVeerisetti888@users.noreply.github.com>
2024-10-15 13:01:47 -0400

    Update welcome_page_locators.py

--- tests/locators/welcome_page_locators.py
+++ tests/locators/welcome_page_locators.py
@@ -2,5 +2,7 @@ from selenium.webdriver.common.by import By
 
 class WelcomePageLocators:
     #TODO Optimize the below locator
+    WelcomeBanner = (By.XPATH, "//h1[contains(text(), 'Welcome Page')]")
     MapLakeViewButton = (By.XPATH, "(//div[contains(@class, 'card boxStyle pointer')])[1]")
+    DataQualityManagement = (By.XPATH, '//a[@href="/DataQualityManagement/"]')
     UsernameBadge = (By.XPATH, "//span[contains(@class, 'cstm-name')]")
