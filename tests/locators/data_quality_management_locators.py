f1293fe8e75996b3a75bf2b00db80319a5262b08
Naga Pavan Veerisetti <63100374+NagaPavanVeerisetti888@users.noreply.github.com>
2024-10-30 10:47:29 -0400

    Update data_quality_management_locators.py

--- tests/locators/data_quality_management_locators.py
+++ tests/locators/data_quality_management_locators.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 
-
 class DataQualityManagementLocators:
     DQMPageBanner = (By.XPATH, "(//*[contains(text(), 'Data Quality Management')])[2]")
     AgencyColumn = (By.XPATH, "//span[contains(text(),'AGENCY')]")
