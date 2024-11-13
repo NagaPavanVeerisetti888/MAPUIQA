a26bed926aea218cca262cb90ca443e98e1ec74e
Naga Pavan Veerisetti <63100374+NagaPavanVeerisetti888@users.noreply.github.com>
2024-11-13 10:56:34 -0500

    Update welcome_page.py

--- tests/pages/welcome_page.py
+++ tests/pages/welcome_page.py
@@ -3,4 +3,16 @@ from tests.pages.page import Page
 
 class WelcomePage(Page):
     def click_MapLakeview(self):
-        self.click(WelcomePageLocators.MapLakeViewButton)
+        #TODO need to remove the print statements after debugging
+        print(f"I am here to debug the code.")
+        title = self.get_page_title()
+        print(f"Title of the page is : {title}")
+        if title == 'MAP Mortgage Asset Positions':
+            print(f"This is before the click on Maplakeview")
+            banner = self.get_element_text(WelcomePageLocators.WelcomeBanner)
+            print(f"This is the banner we got: {banner}")
+            if banner == 'Welcome Page':
+                self.click(WelcomePageLocators.DataQualityManagement)
+                print(f"This is after the click on Maplakeview")
+            else:
+                print(f"driver couldn't find the locator.")
